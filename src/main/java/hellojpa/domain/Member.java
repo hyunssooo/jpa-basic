2ce9a278a050d2d869e6235bd85f0750ff873646
hyunsoojung <jhs695@naver.com>
2020-01-21 01:27:47 +0900

    @Column

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -1,27 +1,34 @@
 package hellojpa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
+
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name")
+    private String userName;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Enumerated(value = EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdDateTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDateTime;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private String temp;
 }
